eb41dcfae8cf963995503946683e1f4a65e4e944
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-05-15 08:24:45 +0200

    Add debug output

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,8 @@
 pushd "%SRC_DIR%"\python
 
 @rem the symlinks for cmake modules don't work here
+dir "%SRC_DIR%\cpp\cmake_modules\
+dir cmake_modules
 copy /Y "%SRC_DIR%\cpp\cmake_modules\BuildUtils.cmake" cmake_modules\
 copy /Y "%SRC_DIR%\cpp\cmake_modules\SetupCxxFlags.cmake" cmake_modules\
 copy /Y "%SRC_DIR%\cpp\cmake_modules\CompilerInfo.cmake" cmake_modules\
