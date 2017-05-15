8606afc1ecf4165b5cb2583df2be1a499a5bb987
Wes McKinney <wes.mckinney@twosigma.com>
2017-05-15 00:21:21 -0400

    Copy cmake file from cpp directory since symlinks haven't been configured properly

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,4 +1,12 @@
 pushd "%SRC_DIR%"\python
+
+@rem the symlinks for cmake modules don't work here
+copy /Y "%SRC_DIR%"\cpp\cmake_modules\BuildUtils.cmake cmake_modules\
+copy /Y "%SRC_DIR%"\cpp\cmake_modules\SetupCxxFlags.cmake cmake_modules\
+copy /Y "%SRC_DIR%"\cpp\cmake_modules\CompilerInfo.cmake cmake_modules\
+copy /Y "%SRC_DIR%"\cpp\cmake_modules\FindNumPy.cmake cmake_modules\
+copy /Y "%SRC_DIR%"\cpp\cmake_modules\FilePythonLibsNew.cmake cmake_modules\
+
 SET ARROW_HOME=%LIBRARY_PREFIX%
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
 if errorlevel 1 exit 1
