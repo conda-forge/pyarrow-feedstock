58174abfb0d103d4c1d8330ef042337e4eeea50f
Korn, Uwe <Uwe.Korn@blue-yonder.com>
2017-05-15 08:14:38 +0200

    Fix quotation

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,11 +1,11 @@
 pushd "%SRC_DIR%"\python
 
 @rem the symlinks for cmake modules don't work here
-copy /Y "%SRC_DIR%"\cpp\cmake_modules\BuildUtils.cmake cmake_modules\
-copy /Y "%SRC_DIR%"\cpp\cmake_modules\SetupCxxFlags.cmake cmake_modules\
-copy /Y "%SRC_DIR%"\cpp\cmake_modules\CompilerInfo.cmake cmake_modules\
-copy /Y "%SRC_DIR%"\cpp\cmake_modules\FindNumPy.cmake cmake_modules\
-copy /Y "%SRC_DIR%"\cpp\cmake_modules\FilePythonLibsNew.cmake cmake_modules\
+copy /Y "%SRC_DIR%\cpp\cmake_modules\BuildUtils.cmake" cmake_modules\
+copy /Y "%SRC_DIR%\cpp\cmake_modules\SetupCxxFlags.cmake" cmake_modules\
+copy /Y "%SRC_DIR%\cpp\cmake_modules\CompilerInfo.cmake" cmake_modules\
+copy /Y "%SRC_DIR%\cpp\cmake_modules\FindNumPy.cmake" cmake_modules\
+copy /Y "%SRC_DIR%\cpp\cmake_modules\FilePythonLibsNew.cmake" cmake_modules\
 
 SET ARROW_HOME=%LIBRARY_PREFIX%
 "%PYTHON%" setup.py install --single-version-externally-managed --record=record.txt
